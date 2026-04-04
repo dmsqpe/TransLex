c222b5fb9c32c4ebe924ddb2538189d34efa3c17
dmsqpe <dms19062011@gmail.com>
2026-04-03 22:44:07 -0300

    pc

---
+++ translex-desktop/src/index.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadURL('https://translex-46nz.onrender.com');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
